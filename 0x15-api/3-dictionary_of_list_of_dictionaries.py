7d6960c34c8e52909fe29bf96f5e47488de47b0c
Girmangus Hailu <girmangus@milkta.com>
2022-11-15 00:25:06 +0300

    0x15-api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
 """Using what you did in the task #0,
    extend your Python script to export
-   data in the JSON format"""
-import json
+   data in the CSV format"""
+import csv
 import requests
+from sys import argv
 
 
 if __name__ == "__main__":
-    URL = "https://jsonplaceholder.typicode.com"
-    users = requests.get(URL + '/users/')
-    todos = requests.get(URL + '/todos')
-    jsonfile = 'todo_all_employees.json'
+    url = "https://jsonplaceholder.typicode.com"
+    userid = int(argv[1])
+    user = requests.get(url + "/users/{}".format(userid))
+    todos = requests.get(url + '/todos')
+    name = user.json().get('username')
+    filename = argv[1] + '.csv'
 
-    data = dict()
-    for user in users.json():
-        userid = user['id']
-        username = user['username']
-        data[str(userid)] = []
+    with open(filename, mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL,
+                            lineterminator='\n')
         for todo in todos.json():
             if todo.get('userId') == userid:
-                data[str(userid)].append(
-                    {
-                        "username": username,
-                        "task": todo['title'],
-                        "completed": todo['completed']
-                    }
-                )
-
-    with open(jsonfile, 'w', newline='') as f:
-        json.dump(data, f)
+                writer.writerow([userid, name, str(todo.get('completed')),
+                                todo.get('title')])
