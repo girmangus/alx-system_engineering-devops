d74e303630ab4c7c0b3cd93762c8bbb72d69e029
Girmangus Hailu <girmangus@milkta.com>
2022-11-30 01:20:17 +0300

    0x16-api_advanced advance task

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,18 +1,61 @@
 #!/usr/bin/python3
-""" recursive function that queries the Reddit API"""
+"""
+Querries the Reddit API and returns a list of the titles of all hot posts
+listed for a given subreddit
+"""
+
+import json
 import requests
-import sys
-after = None
-count_dic = []
 
 
-def count_words(subreddit, word_list):
-    """parses the title of all hot articles, and prints a sorted count of given
-    keywords (case-insensitive, delimited by spaces) """
-    global after
-    global count_dic
-    headers = {'User-Agent': 'alx'}
+def count_words(subreddit, word_list, hot_list=[], after=None):
+    """Return a list of the titles of all hot posts listed for a subreddit"""
+    headers = {"user-agent": "alx"}
+    params = {"after": after}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    response = requests.get(url, headers=headers, allow_redirects=False,
-                            params=parameters)
+    subdata = requests.get(url, headers=headers, params=params)
+    if subdata.status_code != 200:
+        return None
+    data = json.loads(subdata.text).get('data').get('children')
+    after = json.loads(subdata.text).get('data').get('after')
+    if data is None:
+        if len(hot_list) == 0:
+            print("")
+            return
+        word_count = {}
+        word_list = list(set([word.lower() for word in word_list]))
+        for word in word_list:
+            word_count[word] = 0
+        for word in word_list:
+            for title in hot_list:
+                for t in title.split():
+                    if word == t.lower():
+                        word_count[word] = word_count[word] + 1
+        sort_word_count = sorted(word_count.items(), key=lambda x: x[1],
+                                 reverse=True)
+        for i in sort_word_count:
+            if i[1] > 0:
+                print("{}: {}".format(i[0], i[1]))
+    else:
+        for item in data:
+            hot_list.append(item.get('data').get('title'))
+    if after is None:
+        if len(hot_list) == 0:
+            print("")
+            return
+        word_count = {}
+        word_list = list(set([word.lower() for word in word_list]))
+        for word in word_list:
+            word_count[word] = 0
+        for word in word_list:
+            for title in hot_list:
+                for t in title.split():
+                    if word.lower() == t.lower():
+                        word_count[word] = word_count[word] + 1
+        sort_word_count = sorted(word_count.items(), key=lambda x: x[1],
+                                 reverse=True)
+        for i in sort_word_count:
+            if i[1] > 0:
+                print("{}: {}".format(i[0], i[1]))
+    else:
+        return count_words(subreddit, word_list, hot_list, after)
